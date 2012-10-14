a42604da82702c1d062f790c11ff318c75db862a
Kristina Naumova <knaumova@cogniance.com>
2012-10-14 18:19:26 +0300

    kn_commit

---
+++ CourseWork/src/naumova/courework/CourseWork.java
@@ -0,0 +1,35 @@
+package naumova.courework;
+
+public class CourseWork {
+
+		
+	public String readFile(String pathToFile)
+	{
+		return null;
+	}
+	
+	public String getAllHeadersInFile(String fileContent)
+	{
+		return null;
+	}
+	
+	//Some else method that will be declare and implement later
+	/**
+	 * This method must return String which will be pass to result file
+	 * 
+	 * @return
+	 */
+	public String parseDirectory(String pathToCpp, String pathToH)
+	{
+		return null;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
